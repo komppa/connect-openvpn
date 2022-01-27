f42b0be6a361c5baf5241f9f09855a62b59e4cab
komppa <posti@rantakangas.com>
2022-01-27 20:06:18 +0200

    #6 Implemented logger

--- connect-openvpn.py
+++ connect-openvpn.py
@@ -3,9 +3,12 @@ import subprocess
 import netifaces
 import configparser
 import time
+import logging
 
 
 config = configparser.ConfigParser()
+logger = logging.getLogger()
+
 
 ping_servers = [
     "1.1.1.1",
@@ -16,33 +19,44 @@ ping_servers = [
 
 
 def interface_exists(interface_name):
+    logger.info("Checking if interface '{}' exists".format(interface_name))
+        
+
     if interface_name in netifaces.interfaces():
+        logger.debug("Interface '{}' exists".format(interface_name))
         return True
-    else:
-        return False
+
+    logger.debug("Interface {} does not exist".format(interface_name))
+    return False
 
 
 def is_openvpn_running():
+    logger.info("Checking if the OpenVPN client is currently running")
     proc = subprocess.Popen(
         ["ps", "-a"], stdout=subprocess.PIPE
     )
     output = str(proc.communicate())
     if "openvpn" in output:
+        logger.debug("There was keyword 'openvpn' in processes, so OpenVPN client is running")
         return True
-    else:
-        return False
+    logger.debug("There was not keyword 'openvpn' in processes, so OpenVPN client is not running")
+    return False
 
 
 def connect_openvpn(certificate="certificate.ovpn"):
+    logger.info("Connecting to the OpenVPN server using certificte '{}'".format(certificate))
     # Start OpenVPN client, using the ovpn profile. Send output to null and run in background.
     os.system("sudo openvpn --config " + str(certificate) + " > /dev/null &")
+    # TODO get status from os.system and log it as well 
 
 
 def kill_openvpn_client():
+    logger.info("Killing OpenVPN client")
     os.system("sudo pkill -9 openvpn")
 
 
 def ping(address):
+    logger.debug("Pinging server {}".format(address))
     proc = subprocess.Popen(
         ["nc", "-v", "-w", "2", "-z", str(address), "53"], stdout=subprocess.PIPE
     )
@@ -50,10 +64,13 @@ def ping(address):
     if (
         "succeeded" in output
     ):
+        logger.debug("Ping response: \"succeeded\" in output -> True")
         return True
+    logger.debug("Ping response -> False")
     return False
 
 def is_network_up():
+    logger.debug("Checking whether the Internet connection is up")
     # If the fist server says that the connection is up, we can assume that 
     # it is really up. 
     for i, server in enumerate(ping_servers):
@@ -72,20 +89,30 @@ def is_connection_stable():
     """Function for checking if the connection is stable
     """
     # Check if the network is up. 
+    logger.info("Checking if the connection is stable. Calling 'is_network_up()'")
+
     up_status = is_network_up()
+
+    logger.info("Up_status of the network is '{}'".format(up_status))
 
     if up_status:
         # If network is up wait for CONNECTION_STABLE_TIME
+        logger.info("Network was up, waiting '{}'s".format(
+            config["general"]["CONNECTION_STABLE_TIME"]
+        ))
         time.sleep(config["general"]["CONNECTION_STABLE_TIME"])
 
         # Check if network is up again
+        logger.info("Waiting finished. Checking whether the connection is still up")
         up_status = is_network_up()
 
         if up_status:
             # We have stable connection
+            logger.info("Connection was ok, so we have stable connection")
             return True
         else:
             # No stable connection
+            logger.info("Could not establish connection. No stable connection")
             return False
     else:
         # No stable connection
@@ -95,16 +122,25 @@ def main():
     # Read configurations from config.ini
     try:
         config.read_file(open('config.ini'))
+
+        logging.basicConfig(level=logging.DEBUG)
+        logger_h = logging.FileHandler(config['general']['LOG_FILE'])
+        logger_h.setFormatter(logging.Formatter('%(name)s - %(funcName)20s() - %(asctime)s - %(levelname)s - %(message)s'))
+        logger.addHandler(logger_h)
+
     except Exception as e:
-        print("Could not read config.ini file")
+        logger.error("Could not read config.ini file")
         raise
 
     while True:
 
         if is_connection_stable():
 
+            logger.info("Stable connection")
+
             if not is_openvpn_running():
                 #  Network is stable and openvpn is not running, we can connect to VPN
+                logger.info("OpenVPN not running, connecting!")
                 connect_openvpn(config["general"]["CERTIFICATE"])
 
             # Else the connection is stable and vpn is running, so we do nothing
@@ -112,8 +148,11 @@ def main():
         else:
             # Unstable connection
 
+            logger.info("Unstable connection")
+
             if is_openvpn_running():
                 # If the VPN is running, we need to kill it
+                logger.info("OpenVPN running, killing it")
                 kill_openvpn_client()
             
             # If the VPN is not yet running, do nothing
@@ -123,3 +162,4 @@ def main():
 
 if __name__=="__main__":
     main()
+    pass
